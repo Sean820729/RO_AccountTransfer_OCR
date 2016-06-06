881fc75d67bc521d5d251e674fa15a0d9a03913d
Sean820729 <sean820729@gmail.com>
2016-06-06 15:08:38 +0800

    First upload
    1. Account load
    2. Verify code get
    3. OCR

---
+++ OCR_ROTransfer/OCR_ROTransfer/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace OCR_ROTransfer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.FilePath_text = new System.Windows.Forms.TextBox();
+            this.FilePath = new System.Windows.Forms.Button();
+            this.Account_listView = new System.Windows.Forms.ListView();
+            this.VeriImage_picturebox = new System.Windows.Forms.PictureBox();
+            this.VeriCode_textbox = new System.Windows.Forms.TextBox();
+            this.VeriCode_label = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VeriImage_picturebox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.FilePath_text);
+            this.groupBox1.Controls.Add(this.FilePath);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 49);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Accountlist.txt";
+            // 
+            // FilePath_text
+            // 
+            this.FilePath_text.Location = new System.Drawing.Point(6, 19);
+            this.FilePath_text.Name = "FilePath_text";
+            this.FilePath_text.Size = new System.Drawing.Size(139, 22);
+            this.FilePath_text.TabIndex = 1;
+            // 
+            // FilePath
+            // 
+            this.FilePath.Location = new System.Drawing.Point(151, 18);
+            this.FilePath.Name = "FilePath";
+            this.FilePath.Size = new System.Drawing.Size(75, 23);
+            this.FilePath.TabIndex = 0;
+            this.FilePath.Text = "選擇路徑";
+            this.FilePath.UseVisualStyleBackColor = true;
+            this.FilePath.Click += new System.EventHandler(this.FilePath_Click);
+            // 
+            // Account_listView
+            // 
+            this.Account_listView.Location = new System.Drawing.Point(12, 68);
+            this.Account_listView.Name = "Account_listView";
+            this.Account_listView.Size = new System.Drawing.Size(438, 259);
+            this.Account_listView.TabIndex = 4;
+            this.Account_listView.UseCompatibleStateImageBehavior = false;
+            // 
+            // VeriImage_picturebox
+            // 
+            this.VeriImage_picturebox.BackColor = System.Drawing.Color.White;
+            this.VeriImage_picturebox.Location = new System.Drawing.Point(250, 17);
+            this.VeriImage_picturebox.Name = "VeriImage_picturebox";
+            this.VeriImage_picturebox.Size = new System.Drawing.Size(120, 45);
+            this.VeriImage_picturebox.TabIndex = 13;
+            this.VeriImage_picturebox.TabStop = false;
+            this.VeriImage_picturebox.Click += new System.EventHandler(this.VeriImage_picturebox_Click);
+            // 
+            // VeriCode_textbox
+            // 
+            this.VeriCode_textbox.Location = new System.Drawing.Point(376, 40);
+            this.VeriCode_textbox.Name = "VeriCode_textbox";
+            this.VeriCode_textbox.Size = new System.Drawing.Size(74, 22);
+            this.VeriCode_textbox.TabIndex = 21;
+            // 
+            // VeriCode_label
+            // 
+            this.VeriCode_label.AutoSize = true;
+            this.VeriCode_label.Location = new System.Drawing.Point(376, 25);
+            this.VeriCode_label.Name = "VeriCode_label";
+            this.VeriCode_label.Size = new System.Drawing.Size(56, 12);
+            this.VeriCode_label.TabIndex = 22;
+            this.VeriCode_label.Text = "驗  證  碼:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 339);
+            this.Controls.Add(this.VeriCode_label);
+            this.Controls.Add(this.VeriCode_textbox);
+            this.Controls.Add(this.VeriImage_picturebox);
+            this.Controls.Add(this.Account_listView);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.VeriImage_picturebox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox FilePath_text;
+        private System.Windows.Forms.Button FilePath;
+        private System.Windows.Forms.ListView Account_listView;
+        private System.Windows.Forms.PictureBox VeriImage_picturebox;
+        private System.Windows.Forms.TextBox VeriCode_textbox;
+        private System.Windows.Forms.Label VeriCode_label;
+    }
+}
+
---
+++ OCR_ROTransfer/OCR_ROTransfer/Form1.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+using tessnet2;
+
+namespace OCR_ROTransfer
+{
+    public partial class Form1 : Form
+    {
+        CookieContainer myCookieContainer = new CookieContainer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FilePath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            // 預設路徑
+            ofd.Filter = "Config.txt Files (*.txt)|*.txt; *.cpp|All Files (*.*)|*.*";
+            ofd.Title = "請開啟文字檔案";
+
+            if (ofd.ShowDialog(this) == DialogResult.Cancel)
+                return;
+            FilePath_text.Text = ofd.FileName;
+
+            string[] lines = File.ReadAllLines(FilePath_text.Text, Encoding.UTF8);
+            string[][] Account = new string[lines.Length][];
+
+            int Cnt = 1;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i] != "")
+                {
+                    Account[i] = new string[2];
+                    //Account[i][0] = lines[i].Split(new string[] { " 帳號:" }, StringSplitOptions.RemoveEmptyEntries);
+                    string[] tmp = lines[i].Split(new string[] { " 帳號:" }, StringSplitOptions.RemoveEmptyEntries);
+                    tmp = tmp[1].Split(new string[] { " 密碼:" }, StringSplitOptions.RemoveEmptyEntries);
+                    Account[i][0] = tmp[0];
+                    tmp = tmp[1].Split(new string[] { " 遊戲信箱:" }, StringSplitOptions.RemoveEmptyEntries);
+                    Account[i][1] = tmp[0];
+
+                    ListViewItem item = new ListViewItem();
+                    item.SubItems.Clear();
+                    item.SubItems[0].Text = (Cnt++).ToString();
+                    item.SubItems.Add(Account[i][0]);
+                    item.SubItems.Add(Account[i][1]);
+                    item.SubItems.Add("");
+                    Account_listView.Items.Add(item);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Account_listView.GridLines = true;
+            Account_listView.FullRowSelect = true;
+
+            Account_listView.View = View.Details;
+            Account_listView.Scrollable = true;
+            Account_listView.MultiSelect = false;
+
+            Account_listView.Columns.Add("", 50, HorizontalAlignment.Left);
+            Account_listView.Columns.Add("帳號", 160, HorizontalAlignment.Left);
+            Account_listView.Columns.Add("密碼", 160, HorizontalAlignment.Left);
+            Account_listView.Columns.Add("確認", 50, HorizontalAlignment.Left);
+        }
+
+        private void VeriImage_picturebox_Click(object sender, EventArgs e)
+        {
+            GetValidateImage();
+            string Veri = OCR();
+            if (Veri != "")
+            {
+                VeriCode_textbox.Text = Veri;
+            }
+        }
+
+        private void GetValidateImage()
+        {
+            myCookieContainer = new CookieContainer();
+            string url = "https://gfb.gameflier.com/Billing/s_code/spic.aspx";  //验证码页面
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Accept = "*/*";
+            request.Method = "GET";
+            request.UserAgent = "Mozilla/5.0";
+            request.CookieContainer = new CookieContainer(); //暂存到新实例
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream resStream = response.GetResponseStream();//得到验证码数据流
+            Bitmap sourcebm = new Bitmap(resStream);//初始化Bitmap图片
+
+            /*--------------------------------------------------------------------*/
+            for (int i = 0; i < sourcebm.Width; i++)
+            {
+                for (int j = 0; j < sourcebm.Height; j++)
+                {
+                    //获取该点的像素的RGB的颜色  
+                    Color color = sourcebm.GetPixel(i, j);
+                    //利用公式计算灰度值  
+                    int gray = (int)(color.R * 0.3 + color.G * 0.59 + color.B * 0.11);
+                    Color newColor = Color.FromArgb(gray, gray, gray);
+                    sourcebm.SetPixel(i, j, newColor);
+                }
+            }
+            /*--------------------------------------------------------------------*/
+
+            /*--------------------------------------------------------------------*/
+            Bitmap Bmp = sourcebm;
+            Bitmap NewBmp = sourcebm;
+
+            for (int i = 0; i < Bmp.Height; i++) //Pixel Y
+            {
+                for (int j = 0; j < Bmp.Width; j++) //Pixel X
+                {
+                    if (Bmp.GetPixel(j, i).Name == "ff000000" && j < 5 && i > 5)
+                    {
+                        // 向左平移
+                        if (Bmp.GetPixel(0, i).Name == "ff000000")
+                        {
+                            for (int idxCol = 1; idxCol < Bmp.Width; ++idxCol)
+                            {
+                                int idxColDst = idxCol - 1;
+                                Color crSrc = NewBmp.GetPixel(idxCol, i);
+                                NewBmp.SetPixel(idxColDst, i, crSrc);
+                            }
+                        }
+                        // 向右平移
+                        if (Bmp.GetPixel(Bmp.Width - 1, i).Name == "ff000000" && j >= 235 && i > 5)
+                        {
+                            for (int idxCol = Bmp.Width - 2; idxCol >= 0; --idxCol)
+                            {
+                                int idxColDst = idxCol + 1;
+                                Color crSrc = NewBmp.GetPixel(idxCol, i);
+                                NewBmp.SetPixel(idxColDst, i, crSrc);
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < Bmp.Height; i++) //Pixel Y
+            {
+                for (int j = 0; j < Bmp.Width; j++) //Pixel X
+                {
+                    if (Bmp.GetPixel(j, i).Name == "ff000000")
+                    {
+                        // 向上平移
+                        if (Bmp.GetPixel(j, 0).Name == "ff000000")
+                        {
+                            for (int idxRow = 1; idxRow < Bmp.Height; ++idxRow)
+                            {
+                                int idxRowDst = idxRow - 1;
+                                Color crSrc = NewBmp.GetPixel(j, idxRow);
+                                NewBmp.SetPixel(j, idxRowDst, crSrc);
+                            }
+                        }
+                        // 向下平移
+                        if (Bmp.GetPixel(j, Bmp.Height - 1).Name == "ff000000")
+                        {
+                            for (int idxRow = Bmp.Height - 2; idxRow >= 0; --idxRow)
+                            {
+                                int idxRowDst = idxRow + 1;
+                                Color crSrc = NewBmp.GetPixel(j, idxRow);
+                                NewBmp.SetPixel(j, idxRowDst, crSrc);
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < Bmp.Height; i++) //Pixel Y
+            {
+                for (int j = 0; j < Bmp.Width; j++) //Pixel X
+                {
+                    if (Bmp.GetPixel(j, i).Name == "ff000000" && j < 5 && i > 5)
+                    {
+                        // 向左平移
+                        if (Bmp.GetPixel(0, i).Name == "ff000000")
+                        {
+                            for (int idxCol = 1; idxCol < Bmp.Width; ++idxCol)
+                            {
+                                int idxColDst = idxCol - 1;
+                                Color crSrc = NewBmp.GetPixel(idxCol, i);
+                                NewBmp.SetPixel(idxColDst, i, crSrc);
+                            }
+                        }
+                        // 向右平移
+                        if (Bmp.GetPixel(Bmp.Width - 1, i).Name == "ff000000" && j >= 235 && i > 5)
+                        {
+                            for (int idxCol = Bmp.Width - 2; idxCol >= 0; --idxCol)
+                            {
+                                int idxColDst = idxCol + 1;
+                                Color crSrc = NewBmp.GetPixel(idxCol, i);
+                                NewBmp.SetPixel(idxColDst, i, crSrc);
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < Bmp.Height; i++) //Pixel Y
+            {
+                for (int j = 0; j < Bmp.Width; j++) //Pixel X
+                {
+                    if (Bmp.GetPixel(j, i).Name == "ff000000")
+                    {
+                        // 向上平移
+                        if (Bmp.GetPixel(j, 0).Name == "ff000000")
+                        {
+                            for (int idxRow = 1; idxRow < Bmp.Height; ++idxRow)
+                            {
+                                int idxRowDst = idxRow - 1;
+                                Color crSrc = NewBmp.GetPixel(j, idxRow);
+                                NewBmp.SetPixel(j, idxRowDst, crSrc);
+                            }
+                        }
+                        // 向下平移
+                        if (Bmp.GetPixel(j, Bmp.Height - 1).Name == "ff000000")
+                        {
+                            for (int idxRow = Bmp.Height - 2; idxRow >= 0; --idxRow)
+                            {
+                                int idxRowDst = idxRow + 1;
+                                Color crSrc = NewBmp.GetPixel(j, idxRow);
+                                NewBmp.SetPixel(j, idxRowDst, crSrc);
+                            }
+                        }
+                    }
+                }
+            }
+
+            string[,] BackColor = new string[120, 45];
+            string[] RecordColor = new string[3150];
+            int Count = 0;
+
+            for (int i = 0; i < Bmp.Height; i++)
+            {
+                for (int j = 0; j < Bmp.Width; j++)
+                {
+                    BackColor[j, i] = Bmp.GetPixel(j, i).Name;
+                    if (i < 5 || i > 35 || j < 10 || j > 100)
+                    {
+                        for (int k = 0; k < RecordColor.Length; k++)
+                        {
+                            if (Bmp.GetPixel(j, i).Name != RecordColor[k] && RecordColor[k] == null)
+                            {
+                                RecordColor[k] = Bmp.GetPixel(j, i).Name;
+                                Count++;
+                                break;
+                            }
+                            else if (Bmp.GetPixel(j, i).Name == RecordColor[k])
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < Bmp.Height; i++) //Pixel Y
+            {
+                for (int j = 0; j < Bmp.Width; j++) //Pixel X
+                {
+                    for (int k = 0; k < Count; k++)
+                    {
+                        if (Bmp.GetPixel(j, i).Name == RecordColor[k])
+                        {
+                            Bmp.SetPixel(j, i, Color.White);
+                        }
+                    }
+                }
+            }
+            VeriImage_picturebox.Image = Bmp;
+            /*--------------------------------------------------------------------*/
+            foreach (Cookie cook in response.Cookies)
+            {
+                //MessageBox.Show(String.Format("{0} = {1}", cook.Name, cook.Value));
+                if (cook.Name == "scode")
+                {
+                    //String 的Cookie　要轉成　Cookie型的　並放入CookieContainer中
+                    Cookie ck = new Cookie(cook.Name, cook.Value);
+                    ck.Domain = "gfb.gameflier.com";//必須寫對
+                    myCookieContainer.Add(ck);
+                }
+            }
+        }
+
+        private string OCR()
+        {
+            string Veri = "";
+            var ocr = new Tesseract();
+            ocr.SetVariable("tessedit_char_whitelist", "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"); // If digit only
+            //@"C:\OCRTest\tessdata" contains the language package, without this the method crash and app breaks
+            ocr.Init(@".\tessdata", "eng", false);
+            var result = ocr.DoOCR((Bitmap)VeriImage_picturebox.Image, Rectangle.Empty);
+            foreach (Word word in result)
+            {
+                if (word.Text.Length == 4)
+                    Veri = word.Text;
+            }
+            return Veri;
+        }
+    }
+}
